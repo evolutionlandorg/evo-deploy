b703962c6e09a4bc077ec21290fcafeaf480282f
echo <hujw77@gmail.com>
2021-03-02 14:44:31 +0800

    Update WCRING.sol
    fmt

--- src/wrapper/WCRING.sol
+++ src/wrapper/WCRING.sol
@@ -37,14 +37,14 @@ contract WCRING is ERC223 {
     }
     function deposit() public payable {
         balanceOf[msg.sender] += msg.value;
-		emit Transfer(address(0), msg.sender, msg.value);
+	emit Transfer(address(0), msg.sender, msg.value);
         emit Deposit(msg.sender, msg.value);
     }
     function withdraw(uint wad) public {
         require(balanceOf[msg.sender] >= wad);
         balanceOf[msg.sender] -= wad;
         msg.sender.transfer(wad);
-		emit Transfer(msg.sender, address(0), wad);
+	emit Transfer(msg.sender, address(0), wad);
         emit Withdrawal(msg.sender, wad);
     }
 
