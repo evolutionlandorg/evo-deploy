6409d9038641236dd0afd8e48c62dc756364ef49
echo <hujw77@gmail.com>
2021-03-02 14:42:01 +0800

    wring: add Transfer event when deposit and withdraw

--- src/wrapper/WCKTON.sol
+++ src/wrapper/WCKTON.sol
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity ^0.4.18;
+pragma solidity >=0.4.23;
 
 import "../interfaces/ApproveAndCallFallBack.sol";
 import "../interfaces/ERC223ReceivingContract.sol";
@@ -35,18 +35,20 @@ contract WCKTON is ERC223 {
     mapping (address => mapping (address => uint)) public allowance;
 
     function deposit(address from, uint256 value) public {
-	require(msg.sender == KTON_PRECOMPILE, "WKTON: PERMISSION");
-	totalSupply += value;
+        require(msg.sender == KTON_PRECOMPILE, "WKTON: PERMISSION");
+        totalSupply += value;
         balanceOf[from] += value;
-        Deposit(from, value);
+		emit Transfer(address(0), from, value);
+        emit Deposit(from, value);
     }
     function withdraw(bytes32 to, uint wad) public {
         require(balanceOf[msg.sender] >= wad);
-	totalSupply -= wad;
+        totalSupply -= wad;
         balanceOf[msg.sender] -= wad;
-	bool success = KTON_PRECOMPILE.call(bytes4(keccak256("withdraw(bytes32,uint256)")), to, wad);
-	require(success, "WKTON: WITHDRAW_FAILED");
-        Withdrawal(to, wad);
+        bool success = KTON_PRECOMPILE.call(bytes4(keccak256("withdraw(bytes32,uint256)")), to, wad);
+        require(success, "WKTON: WITHDRAW_FAILED");
+		emit Transfer(msg.sender, address(0), wad);
+        emit Withdrawal(to, wad);
     }
 
     function totalSupply() public view returns (uint) {
@@ -55,7 +57,7 @@ contract WCKTON is ERC223 {
 
     function approve(address guy, uint wad) public returns (bool) {
         allowance[msg.sender][guy] = wad;
-        Approval(msg.sender, guy, wad);
+        emit Approval(msg.sender, guy, wad);
         return true;
     }
 
@@ -77,7 +79,7 @@ contract WCKTON is ERC223 {
         balanceOf[src] -= wad;
         balanceOf[dst] += wad;
 
-        Transfer(src, dst, wad);
+        emit Transfer(src, dst, wad);
 
         return true;
     }
@@ -116,7 +118,7 @@ contract WCKTON is ERC223 {
             ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
             receiver.tokenFallback(_from, _amount, _data);
         }
-        ERC223Transfer(_from, _to, _amount, _data);
+        emit ERC223Transfer(_from, _to, _amount, _data);
         return true;
     }
 
--- src/wrapper/WCRING.sol
+++ src/wrapper/WCRING.sol
@@ -1,4 +1,4 @@
-// Copyright (C) 2015, 2016, 2017 Dapphub
+// Copyright (C) 2015, 2016, 2017, 2019 Dapphub
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity ^0.4.18;
+pragma solidity >=0.4.23;
 
 import "../interfaces/ApproveAndCallFallBack.sol";
 import "../interfaces/ERC223ReceivingContract.sol";
@@ -37,22 +37,24 @@ contract WCRING is ERC223 {
     }
     function deposit() public payable {
         balanceOf[msg.sender] += msg.value;
-        Deposit(msg.sender, msg.value);
+		emit Transfer(address(0), msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
     }
     function withdraw(uint wad) public {
         require(balanceOf[msg.sender] >= wad);
         balanceOf[msg.sender] -= wad;
         msg.sender.transfer(wad);
-        Withdrawal(msg.sender, wad);
+		emit Transfer(msg.sender, address(0), wad);
+        emit Withdrawal(msg.sender, wad);
     }
 
     function totalSupply() public view returns (uint) {
-        return this.balance;
+        return address(this).balance;
     }
 
     function approve(address guy, uint wad) public returns (bool) {
         allowance[msg.sender][guy] = wad;
-        Approval(msg.sender, guy, wad);
+        emit Approval(msg.sender, guy, wad);
         return true;
     }
 
@@ -74,7 +76,7 @@ contract WCRING is ERC223 {
         balanceOf[src] -= wad;
         balanceOf[dst] += wad;
 
-        Transfer(src, dst, wad);
+        emit Transfer(src, dst, wad);
 
         return true;
     }
@@ -113,7 +115,7 @@ contract WCRING is ERC223 {
             ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
             receiver.tokenFallback(_from, _amount, _data);
         }
-        ERC223Transfer(_from, _to, _amount, _data);
+        emit ERC223Transfer(_from, _to, _amount, _data);
         return true;
     }
 
